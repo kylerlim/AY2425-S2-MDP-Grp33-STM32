f05464bf6ee46ac97f86385f1a38bc0b11620260
kylerlim <zlim132@e.ntu.edu.sg>
2025-03-10 00:50:36 +0800

    Added encoder and motor tasks, PID_Control functions

--- Movement/Core/Src/main.c
+++ Movement/Core/Src/main.c
@@ -33,13 +33,35 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-uint16_t pwmVal_servo;
+
+// SERVO-MOTOR
 uint16_t SERVO_STRAIGHT = 146;
 uint16_t SERVO_LEFT = 72;
-uint16_t SERVO_RIGHT = 186; // original at 220
+uint16_t SERVO_RIGHT = 186; // original at 220, Right turn has a handicap
+uint16_t pwmVal_servo;
+
+// hard-code TO-DELETE
 uint16_t DC_RIGHT = 2100;
 uint16_t DC_LEFT = 1670;
+//
 
+// PID-controlled DC Motors
+#define COUNT_PER_REV 1320  // Quadrature-encoded pulses per wheel revolution
+
+const double PI = 3.141592653;
+const double WHEEL_DIAMETER = 7.0;  // cm
+const double WHEEL_CIRCUM = WHEEL_DIAMETER * PI;
+const double COUNT_PER_CM = COUNT_PER_REV / WHEEL_CIRCUM;
+
+
+// PID variables
+float dTRight, dTLeft;
+
+// REAR WHEELS ENCODER VARIABLES
+int32_t rightEncoderVal = 0, leftEncoderVal = 0;
+int32_t rightTargetVal = 0, leftTargetVal = 0;
+int32_t rightErrorVal = 0, leftErrorVal = 0;
+int32_t rightIntegral = 0, leftIntegral = 0;
 
 uint16_t ANGLE_TESTING_PHASE = 0;
 
@@ -71,19 +93,33 @@ TIM_HandleTypeDef htim8;
 UART_HandleTypeDef huart1;
 UART_HandleTypeDef huart3;
 
-/* Definitions for USART3Rx */
-osThreadId_t USART3RxHandle;
-const osThreadAttr_t USART3Rx_attributes = {
-  .name = "USART3Rx",
+/* Definitions for USART1Rx */
+osThreadId_t USART1RxHandle;
+const osThreadAttr_t USART1Rx_attributes = {
+  .name = "USART1Rx",
   .stack_size = 128 * 4,
   .priority = (osPriority_t) osPriorityNormal,
 };
-/* Definitions for turnCheck */
-osThreadId_t turnCheckHandle;
-const osThreadAttr_t turnCheck_attributes = {
-  .name = "turnCheck",
+/* Definitions for encLeftTask */
+osThreadId_t encLeftTaskHandle;
+const osThreadAttr_t encLeftTask_attributes = {
+  .name = "encLeftTask",
   .stack_size = 128 * 4,
   .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for encRightTask */
+osThreadId_t encRightTaskHandle;
+const osThreadAttr_t encRightTask_attributes = {
+  .name = "encRightTask",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for motorTask */
+osThreadId_t motorTaskHandle;
+const osThreadAttr_t motorTask_attributes = {
+  .name = "motorTask",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
 };
 /* USER CODE BEGIN PV */
 uint8_t task1 [30] = "Task1";
@@ -101,8 +137,10 @@ static void MX_TIM6_Init(void);
 static void MX_TIM4_Init(void);
 static void MX_TIM2_Init(void);
 static void MX_TIM3_Init(void);
-void USART3Receive(void *argument);
-void turnChecks(void *argument);
+void USART1Receive(void *argument);
+void encoderLeftTask(void *argument);
+void encoderRightTask(void *argument);
+void dcMotorTask(void *argument);
 
 /* USER CODE BEGIN PFP */
 
@@ -114,6 +152,8 @@ void turnChecks(void *argument);
 volatile uint8_t aRxBuffer [CMD_MAX_LENGTH];
 volatile uint8_t bufferIndex = 0;
 
+
+// RPI
 int flagDone = 0;
 char key;
 char direction;
@@ -188,11 +228,17 @@ int main(void)
   /* USER CODE END RTOS_QUEUES */
 
   /* Create the thread(s) */
-  /* creation of USART3Rx */
-  USART3RxHandle = osThreadNew(USART3Receive, NULL, &USART3Rx_attributes);
+  /* creation of USART1Rx */
+  USART1RxHandle = osThreadNew(USART1Receive, NULL, &USART1Rx_attributes);
 
-  /* creation of turnCheck */
-  turnCheckHandle = osThreadNew(turnChecks, NULL, &turnCheck_attributes);
+  /* creation of encLeftTask */
+  encLeftTaskHandle = osThreadNew(encoderLeftTask, NULL, &encLeftTask_attributes);
+
+  /* creation of encRightTask */
+  encRightTaskHandle = osThreadNew(encoderRightTask, NULL, &encRightTask_attributes);
+
+  /* creation of motorTask */
+  motorTaskHandle = osThreadNew(dcMotorTask, NULL, &motorTask_attributes);
 
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
@@ -346,10 +392,10 @@ static void MX_TIM2_Init(void)
   htim2.Instance = TIM2;
   htim2.Init.Prescaler = 0;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 65536;
+  htim2.Init.Period = 65535;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  sConfig.EncoderMode = TIM_ENCODERMODE_TI1;
+  sConfig.EncoderMode = TIM_ENCODERMODE_TI12;
   sConfig.IC1Polarity = TIM_ICPOLARITY_RISING;
   sConfig.IC1Selection = TIM_ICSELECTION_DIRECTTI;
   sConfig.IC1Prescaler = TIM_ICPSC_DIV1;
@@ -1074,31 +1120,59 @@ void driveBackward(int distance_in_cm) {
 	  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_4);
 	  HAL_TIM_PWM_Start(&htim8, TIM_CHANNEL_1);
 	  HAL_TIM_PWM_Start(&htim8, TIM_CHANNEL_2);
+int PID_Control(uint8_t right0Left1) {
+  // PID gains
+  const float Kp = 1.0, Ki = 0.0, Kd = 0.0;
+  float derivative;
+  float MAX_INTEGRAL = 10;
+  
+  // Select variables based on right0Left1 flag
+  float dT = (right0Left1 == 1) ? dTLeft : dTRight;
+  int* errorVal = (right0Left1 == 1) ? &leftErrorVal : &rightErrorVal;
+  int* encoderVal = (right0Left1 == 1) ? &leftEncoderVal : &rightEncoderVal;
+  int* targetVal = (right0Left1 == 1) ? &leftTargetVal : &rightTargetVal;
+  float* integral = (right0Left1 == 1) ? &leftIntegral : &rightIntegral;
+  GPIO_PinState pin1, pin2;
+  
+  // Calculate PID components
+  int prevError = *errorVal;
+  *errorVal = *encoderVal - *targetVal;
 
-	  /* Turn the wheels left */
-	  htim1.Instance->CCR4 = SERVO_STRAIGHT;
+  if (dT > 0) {
+      derivative = (*errorVal - prevError) / dT;
+  } else {
+      derivative = 0;
+  }
 
-	  /* Configure RIGHT wheel to cause forward motion of car */
-	  HAL_GPIO_WritePin(GPIOA, AIN2_Pin, GPIO_PIN_RESET);
-	  HAL_GPIO_WritePin(GPIOA, AIN1_Pin, GPIO_PIN_SET);
-	  /* Configure LEFT wheel to cause forward motion of car */
-	  HAL_GPIO_WritePin(GPIOA, BIN2_Pin, GPIO_PIN_RESET);
-	  HAL_GPIO_WritePin(GPIOA, BIN1_Pin, GPIO_PIN_SET);
+  *integral += (*errorVal * dT);
+
+  if (*integral > MAX_INTEGRAL) *integral = MAX_INTEGRAL;
+  if (*integral < -MAX_INTEGRAL) *integral = -MAX_INTEGRAL;
 
 
-	  __HAL_TIM_SetCompare(&htim8, TIM_CHANNEL_1, pwmVal);
-	  __HAL_TIM_SetCompare(&htim8, TIM_CHANNEL_2, pwmVal);
+  // Compute control output
+  float u_float = Kp * (*errorVal) + Kd * derivative + Ki * (*integral);
+  int u = (int) u_float; // Convert to integer
 
-	  /*Dynamic function to cause different degree turn*/
-	  osDelay(MILLISECONDS_PER_CENTIMETRE * distance_in_cm);
+  // Determine motor direction
+  if (u > 0) {
+      pin1 = GPIO_PIN_SET;
+      pin2 = GPIO_PIN_RESET;
+  } else {
+      pin1 = GPIO_PIN_RESET;
+      pin2 = GPIO_PIN_SET;
+  }
 
-	  /* Reset servo to straight and stop motors */
-	  htim1.Instance->CCR4 = SERVO_STRAIGHT;
-	  __HAL_TIM_SetCompare(&htim8, TIM_CHANNEL_1, 0);
-	  __HAL_TIM_SetCompare(&htim8, TIM_CHANNEL_2, 0);
+  // Apply motor control based on selection
+  if (right0Left1 == 1) {
+      HAL_GPIO_WritePin(GPIOA, AIN2_Pin, pin1);
+      HAL_GPIO_WritePin(GPIOA, AIN1_Pin, pin2);
+  } else {
+      HAL_GPIO_WritePin(GPIOA, BIN2_Pin, pin1);
+      HAL_GPIO_WritePin(GPIOA, BIN1_Pin, pin2);
+  }
 
-	  // For Testing purposes, adjust accordingly during release
-	  osDelay(2000);
+  return abs(u);
 }
 
 void stopCar() {
@@ -1179,39 +1253,27 @@ uint16_t ultrasonic()
     return distance_return;
 }
 
-uint16_t readIRSensor(ADC_HandleTypeDef *hadc) {
-    char buf[20];
-
-    HAL_ADC_Start(hadc);
-    HAL_ADC_PollForConversion(hadc, 10);
-    uint16_t ADC_VAL = HAL_ADC_GetValue(hadc);
-    HAL_ADC_Stop(hadc);
-
-    // Display ADC value
-    sprintf(buf, "ADC Val: %5d", ADC_VAL);
-    OLED_ShowString(10, 10, buf);
-
-    // Compute and display distance
-    uint16_t distance = 136.68 * exp (924.195/ ADC_VAL) - 140.33;
-//    		distanceCalc(ADC_VAL);
-    sprintf(buf, "Dist: %5d", distance);
-    OLED_ShowString(10, 30, buf);
-    OLED_Refresh_Gram();
-
-    return ADC_VAL;
-}
-
-void turnToNextFace(){
-
-	driveForward(10);
-	driveBackward(20);
-	turnLeftNarrow(90);
-	driveForward(30);
-	turnRightNarrow(100);
-	driveForward(15);
-	turnRightNarrow(100);
-
-}
+//uint16_t readIRSensor(ADC_HandleTypeDef *hadc) {
+//    char buf[20];
+//
+//    HAL_ADC_Start(hadc);
+//    HAL_ADC_PollForConversion(hadc, 10);
+//    uint16_t ADC_VAL = HAL_ADC_GetValue(hadc);
+//    HAL_ADC_Stop(hadc);
+//
+//    // Display ADC value
+//    sprintf(buf, "ADC Val: %5d", ADC_VAL);
+//    OLED_ShowString(10, 10, buf);
+//
+//    // Compute and display distance
+//    uint16_t distance = 136.68 * exp (924.195/ ADC_VAL) - 140.33;
+////    		distanceCalc(ADC_VAL);
+//    sprintf(buf, "Dist: %5d", distance);
+//    OLED_ShowString(10, 30, buf);
+//    OLED_Refresh_Gram();
+//
+//    return ADC_VAL;
+//}
 
 void delay_us(uint16_t us){
 	HAL_TIM_Base_Start(&htim6);
@@ -1253,57 +1315,13 @@ void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
     }
 }
 
-void threePointTurn(){
-	 turnLeftReverse(90);
-	 turnRightNarrow(90);
-
-}
-
-void try(){
-	 turnRightNarrow(90);
-	 driveForward(20);
-	 turnLeftNarrow(90);
-	 turnLeftNarrow(90);
-
-	 turnRightNarrow(90);
-	 driveForward(20);
-	 turnLeftNarrow(90);
-	 turnLeftNarrow(90);
-
-	 turnRightNarrow(90);
-	 driveForward(20);
-	 turnLeftNarrow(90);
-	 turnLeftNarrow(90);
-
-	 turnRightNarrow(90);
-	 driveForward(20);
-	 turnLeftNarrow(90);
-	 turnLeftNarrow(90);
-
-
-}
-
-//void start_uart_receive(void) {
-//    // Start receiving CMD_MAX_LENGTH bytes.
-//    HAL_UART_Receive_IT(&huart1, (uint8_t *)aRxBuffer, CMD_MAX_LENGTH);
-//}
-
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
 	UNUSED(huart);
-	
-  // for debugging
-  // HAL_UART_Transmit(&huart1, (uint8_t *) aRxBuffer, CMD_MAX_LENGTH, 0xFFFF);
-
-
     OLED_ShowString(10, 20, aRxBuffer); //sanity check
-
     OLED_Refresh_Gram();
-
   	readyToExecute = 1;
-
 	HAL_UART_Receive_IT(&huart1, aRxBuffer, CMD_MAX_LENGTH);
-  
 }
 
 void acknowledgeCompletion(){
@@ -1314,14 +1332,14 @@ void acknowledgeCompletion(){
 
 /* USER CODE END 4 */
 
-/* USER CODE BEGIN Header_USART3Receive */
+/* USER CODE BEGIN Header_USART1Receive */
 /**
-  * @brief  Function implementing the USART3Rx thread.
+  * @brief  Function implementing the USART1Rx thread.
   * @param  argument: Not used
   * @retval None
   */
-/* USER CODE END Header_USART3Receive */
-void USART3Receive(void *argument)
+/* USER CODE END Header_USART1Receive */
+void USART1Receive(void *argument)
 {
   /* USER CODE BEGIN 5 */
 	char ch = 'A';
@@ -1442,7 +1460,7 @@ void USART3Receive(void *argument)
 
 					  case '-':
 						  times_acceptable=0;
-						  stopCar();
+//						  stopCar();
 	//					  while(finishCheck());
 						  flagDone=1;
 	//					  memset(aRxBuffer, 0 , CMD_MAX_LENGTH);
@@ -1475,24 +1493,143 @@ void USART3Receive(void *argument)
   /* USER CODE END 5 */
 }
 
-/* USER CODE BEGIN Header_turnChecks */
+/* USER CODE BEGIN Header_encoderLeftTask */
 /**
-* @brief Function implementing the turnCheck thread.
+* @brief Function implementing the encLeftTask thread.
 * @param argument: Not used
 * @retval None
 */
-/* USER CODE END Header_turnChecks */
-void turnChecks(void *argument)
+/* USER CODE END Header_encoderLeftTask */
+void encoderLeftTask(void *argument)
 {
-  /* USER CODE BEGIN turnChecks */
-
+  /* USER CODE BEGIN encoderLeftTask */
+	// using motor B
+	TIM_HandleTypeDef *htim = &htim3;
+	HAL_TIM_Encoder_Start(htim,TIM_CHANNEL_ALL);
+	int16_t cnt_B;
+	int8_t dirL = 1;
+	int diff = 0;
+	uint32_t tick = HAL_GetTick();
 
   /* Infinite loop */
   for(;;)
   {
 
+	  if (HAL_GetTick() - tick > 10L){ // 10ms delay
+		  dTLeft =  (HAL_GetTick() - tick) / 1000;
+		  cnt_B = __HAL_TIM_GET_COUNTER(htim);
+		  if (__HAL_TIM_IS_TIM_COUNTING_DOWN(htim)){ // moving forward
+			  dirL = 1;
+			  diff = 65536 - cnt_B;
+		  }
+		  else{
+				dirL = -1;
+				diff = cnt_B;
+		  }
+
+		  if(dirL == 1){
+			  leftEncoderVal += (int32_t)diff;
+		  }
+
+		  else{
+			  leftEncoderVal -= (int32_t)diff;
+		  }
+		  __HAL_TIM_SET_COUNTER(htim, 0);
+
+		  tick = HAL_GetTick();
+	  }
+//    osDelay(1);
   }
-  /* USER CODE END turnChecks */
+  /* USER CODE END encoderLeftTask */
+}
+
+/* USER CODE BEGIN Header_encoderRightTask */
+/**
+* @brief Function implementing the encRightTask thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_encoderRightTask */
+void encoderRightTask(void *argument)
+{
+  /* USER CODE BEGIN encoderRightTask */
+	// using motor A
+	TIM_HandleTypeDef *htim = &htim2;
+	HAL_TIM_Encoder_Start(htim,TIM_CHANNEL_ALL);
+	int16_t cnt_A;
+	int8_t dirR = 1;
+	int diff = 0;
+	uint32_t tick = HAL_GetTick();
+
+  /* Infinite loop */
+  for(;;)
+  {
+
+	  if (HAL_GetTick() - tick > 10L){ // 10ms delay
+		  dTRight = (HAL_GetTick() - tick) / 1000;
+		  cnt_A = __HAL_TIM_GET_COUNTER(htim);
+		  if (__HAL_TIM_IS_TIM_COUNTING_DOWN(htim)){ // moving forward
+			dirR = 1;
+			diff = 65536 - cnt_A;
+		  }
+		  else{
+				dirR = -1;
+				diff = cnt_A;
+		  }
+
+		  if(dirR == 1){
+			  rightEncoderVal += (int32_t)diff;
+		  }
+
+		  else{
+			  rightEncoderVal -= (int32_t)diff;
+		  }
+		  __HAL_TIM_SET_COUNTER(htim, 0);
+
+		  tick = HAL_GetTick();
+	  }
+//    osDelay(1);
+  }
+  /* USER CODE END encoderRightTask */
+}
+
+/* USER CODE BEGIN Header_dcMotorTask */
+/**
+* @brief Function implementing the motorTask thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_dcMotorTask */
+void dcMotorTask(void *argument)
+{
+  /* USER CODE BEGIN dcMotorTask */
+
+	  /*
+	  Initialise Timer and Channels
+	  Timer 1 Channel 4 for Servo Motor FRONT Steer
+	  Timer 8 Channel 1 for Motor A -- configured for RIGHT DC Wheel
+	  Timer 8 Channel 2 for Motor B -- configured for LEFT DC Wheel
+	  */
+	  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_4);
+	  HAL_TIM_PWM_Start(&htim8, TIM_CHANNEL_1);
+	  HAL_TIM_PWM_Start(&htim8, TIM_CHANNEL_2);
+
+	  /* Configure RIGHT wheel to cause forward motion of car */
+	  HAL_GPIO_WritePin(GPIOA, AIN2_Pin, GPIO_PIN_SET);
+	  HAL_GPIO_WritePin(GPIOA, AIN1_Pin, GPIO_PIN_RESET);
+	  /* Configure LEFT wheel to cause forward motion of car */
+	  HAL_GPIO_WritePin(GPIOA, BIN2_Pin, GPIO_PIN_SET);
+	  HAL_GPIO_WritePin(GPIOA, BIN1_Pin, GPIO_PIN_RESET);
+
+
+  /* Infinite loop */
+  for(;;)
+  {
+	  __HAL_TIM_SetCompare(&htim8, TIM_CHANNEL_1, PID_Control(0));
+	  __HAL_TIM_SetCompare(&htim8, TIM_CHANNEL_2, PID_Control(1));
+	  osDelay(10);
+  }
+  /* USER CODE END dcMotorTask */
 }
 
 /**
